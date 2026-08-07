9733f5910ed70375055fd2dec3aa308a3f524834
Liam Healey <105923500@student.swin.edu.au>
2026-08-07 13:50:27 +1000

    Initial test commit

---
+++ SampleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deployment Task Pass Level Test!");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
