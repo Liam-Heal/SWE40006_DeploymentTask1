89ecb5e139603a2a1eacba26e85e486def73b8c3
Liam Healey <105923500@student.swin.edu.au>
2026-08-14 12:52:02 +1000

    NewtonJSON Dependency added

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace SampleApp
 {
@@ -6,7 +7,12 @@ namespace SampleApp
     {
         static void Main(string[] args)
         {
+            var data = new { Message = "Deployment Task Test", Level = "Distinction (1.3)" };
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+
             Console.WriteLine("Deployment Task Pass Level Test!");
+            Console.WriteLine("Using Newtonsoft.Json to serialize sample data:");
+            Console.WriteLine(json);
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
